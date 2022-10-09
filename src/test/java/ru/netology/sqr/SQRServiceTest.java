858ce39bfac2ea630977dafb1f28c50351e98a81
Shkil <lyuda.shckil@yandex.ru>
2022-10-09 12:58:29 +0300

    1

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,57 @@
+package ru.netology.sqr;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void test() {
+
+        SQRService service = new SQRService();
+        int actual = service.calculate(200, 300);
+        int expected = 3;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void test2() {
+
+        SQRService service = new SQRService();
+        int actual = service.calculate(200, 200);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void test3() {
+
+        SQRService service = new SQRService();
+        int actual = service.calculate(0, 300);
+        int expected = 8;
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void test4() {
+
+        SQRService service = new SQRService();
+        int actual = service.calculate(300, 300);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+
+
+    }
+}
